d1e645c66539f20b2136a91f4665396d9e081d7c
goldenstein64 <goldnstein64@gmail.com>
2024-04-13 12:12:19 -0400

    style: edited console output

--- App/Program.cs
+++ App/Program.cs
@@ -9,15 +9,14 @@ IConnection connection = new ConsoleConnection(
 		[IOMessages.MSG_PromptPlayer] =
 			"Is Player {0} a player or computer? [H/C]: ",
 
-		[IOMessages.ERR_ComputerInvalid] =
-			"This does not match 'E', 'M' or 'H'!",
+		[IOMessages.ERR_ComputerInvalid] = "This does not match 'E', 'M' or 'H'!",
 		[IOMessages.MSG_PromptComputer] =
 			"What is computer {0}'s difficulty? [E/M/H]: ",
 
 		[IOMessages.MSG_PlayerWon] = "Player {0} won!",
 		[IOMessages.MSG_Tied] = "There was a tie!",
 
-		[IOMessages.MSG_Board] = "{0}",
+		[IOMessages.MSG_Board] = "{0}\n",
 
 		[IOMessages.MSG_PromptMove] = "Pick a move, Player {0} [1-9]: ",
 		[IOMessages.ERR_NotANumber] = "This is not a valid number!",
