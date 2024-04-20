c6ab7d95c21f25187a51793dcdf3f5e0fd64fe8c
goldenstein64 <goldnstein64@gmail.com>
2024-04-20 01:27:50 -0400

    test: parameterized CommonTactics

--- Tests/src/Player/CommonTactics.cs
+++ Tests/src/Player/CommonTactics.cs
@@ -5,135 +5,43 @@ namespace TicTacToe.Tests.Player.CommonTacticsTests;
 
 public class CommonTactics
 {
-	public static IEnumerable<object[]> GenerateComputers() =>
+	public static IEnumerable<object[]> ComputerCases() =>
 		[
 			[new MediumComputer()],
 			[new HardComputer()],
 		];
 
-	[Theory]
-	[MemberData(nameof(GenerateComputers))]
-	public void DetectsXWinningMove(IPlayer computer)
+	public static IEnumerable<object[]> TestCases =
+	[ // pattern, mark, expected
+		[",XX,OO,,,", Mark.X, 0], // X winning move
+		[",OO,XX,,X", Mark.O, 0], // O winning move
+		["O,,O,X,X,", Mark.X, 6], // X blocking move
+		[",O,X,,XXO", Mark.O, 0], // O blocking move
+		[",X,O,X,O,", Mark.X, 2], // X trapping move
+		[",X,OXX,O,", Mark.O, 6], // O trapping move
+		[",XXXOOOX,", Mark.O, 0],
+	];
+
+	public static IEnumerable<object[]> GenerateCases()
 	{
-		/*
-		   0 | X | X
-		  ---|---|---
-		   3 | O | O
-		  ---|---|---
-		   6 | 7 | 8
-		*/
-		var board = new Board(",XX,OO,,,");
-
-		var move = computer.GetMove(board, Mark.X);
-
-		Assert.Equal(0, move);
+		foreach (object[] testArgs in TestCases)
+		foreach (object[] cpuArgs in ComputerCases())
+			yield return [.. cpuArgs, .. testArgs];
 	}
 
 	[Theory]
-	[MemberData(nameof(GenerateComputers))]
-	public void DetectsOWinningMove(IPlayer computer)
+	[MemberData(nameof(GenerateCases))]
+	public void PassesAllTests(
+		IPlayer computer,
+		string pattern,
+		Mark mark,
+		int expected
+	)
 	{
-		/*
-		   0 | O | O
-		  ---|---|---
-		   3 | X | X
-		  ---|---|---
-		   6 | 7 | X
-		*/
-		var board = new Board(",OO,XX,,X");
+		var board = new Board(pattern);
 
-		var move = computer.GetMove(board, Mark.O);
+		var move = computer.GetMove(board, mark);
 
-		Assert.Equal(0, move);
-	}
-
-	[Theory]
-	[MemberData(nameof(GenerateComputers))]
-	public void DetectsXBlockingMove(IPlayer computer)
-	{
-		/*
-		   O | 1 | 2
-		  ---|---|---
-		   O | 4 | X
-		  ---|---|---
-		   6 | X | 8
-		*/
-		var board = new Board("O,,O,X,X,");
-
-		var move = computer.GetMove(board, Mark.X);
-
-		Assert.Equal(6, move);
-	}
-
-	[Theory]
-	[MemberData(nameof(GenerateComputers))]
-	public void DetectsOBlockingMove(IPlayer computer)
-	{
-		/*
-		   0 | O | 2
-		  ---|---|---
-		   X | 4 | 5
-		  ---|---|---
-		   X | X | O
-		*/
-		var board = new Board(",O,X,,XXO");
-
-		var move = computer.GetMove(board, Mark.O);
-
-		Assert.Equal(0, move);
-	}
-
-	[Theory]
-	[MemberData(nameof(GenerateComputers))]
-	public void DetectsXTrappingMove(IPlayer computer)
-	{
-		/*
-		   0 | X | 2
-		  ---|---|---
-		   O | 4 | X
-		  ---|---|---
-		   6 | O | 8
-		*/
-		var board = new Board(",X,O,X,O,");
-
-		var move = computer.GetMove(board, Mark.X);
-
-		Assert.Equal(2, move);
-	}
-
-	[Theory]
-	[MemberData(nameof(GenerateComputers))]
-	public void DetectsOTrappingMove(IPlayer computer)
-	{
-		/*
-		   0 | X | 2
-		  ---|---|---
-		   O | X | X
-		  ---|---|---
-		   6 | O | 8
-		*/
-		var board = new Board(",X,OXX,O,");
-
-		var move = computer.GetMove(board, Mark.O);
-
-		Assert.Equal(6, move);
-	}
-
-	[Theory]
-	[MemberData(nameof(GenerateComputers))]
-	public void Matches_XXXOOOX_(IPlayer computer)
-	{
-		/*
-		   0 | X | X
-		  ---|---|---
-		   X | O | O
-		  ---|---|---
-		   O | X | 8
-		*/
-		var board = new Board(",XXXOOOX,");
-
-		var move = computer.GetMove(board, Mark.O);
-
-		Assert.Equal(0, move);
+		Assert.Equal(expected, move);
 	}
 }
