ccf42149db2601c28192213f60c7871d1c13ed06
goldenstein64 <goldnstein64@gmail.com>
2025-01-03 21:36:23 -0500

    feat(Mark): use pattern matching

--- Lib/src/Data/Mark.cs
+++ Lib/src/Data/Mark.cs
@@ -10,11 +10,11 @@ public static class MarkExtensions
 {
 	public static Mark Other(this Mark mark)
 	{
-		return mark == Mark.X ? Mark.O : Mark.X;
+		return mark is Mark.X ? Mark.O : Mark.X;
 	}
 
 	public static string ToString(this Mark mark)
 	{
-		return mark == Mark.X ? "O" : "X";
+		return mark is Mark.X ? "O" : "X";
 	}
 }
